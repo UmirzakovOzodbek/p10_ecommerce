e858230773f861c30113b60ec5fe1ead7e4f834c
user <ozodbekumurzakov2006@gmail.com>
2023-04-28 16:14:29 +0500

    Initial commit .gitignore

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
