f245816126db79c4582ef91a4669ad7838dac74e
user <ozodbekumurzakov2006@gmail.com>
2023-04-28 16:46:19 +0500

    Initial commit .gitignore

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -137,9 +137,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
